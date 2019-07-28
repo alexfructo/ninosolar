dc8ea8bbf4221dd1c9c3483dae781204dfb5e2b0
Alex Fructo <alexfructo@gmail.com>
2019-07-28 10:54:02 -0300

    Removed unnecessary prototype files, added new information and settings to the page.

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -16,7 +16,7 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
    
 // Create the email and send the message
-$to = 'naoresponda@ninosolar.com.br'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'contato@ninosolar.com.br'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Formulário de contato do site:  $name";
 $email_body = "Você recebeu uma nova mensagem do formulário de contato do seu site.\n\n"."Aqui estão os detalhes:\n\nNome: $name\n\nEmail: $email_address\n\nTelefone: $phone\n\nMensagem:\n$message";
 $headers = "From: naoresponda@ninosolar.com.br\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
